a221afc655355e75f2bf7b7b3c7f9027a0177c84
Max <maksimatsiuk456@gmail.com>
2026-03-29 19:26:54 +0300

    Completed practice Hotel_Manager

--- Lops/While_Lops/Hotel_Manager.py
+++ Lops/While_Lops/Hotel_Manager.py
@@ -1,7 +1,7 @@
 import os
 
 def clear_screen():
-        os.system('cls' if os.name == 'nt' else 'clear')
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 all_rooms = []
 for floors in range(1, 6):    
@@ -37,33 +37,86 @@ while True:
             break     
         
         elif booking == 103 or booking == 304: 
-            input("Извините за неудобство, но данный момент этот номер на ремонте. С уважением администрация\n\nНажмите нажмите любую клавишу, чтобы продолжить...")
+            print("-" * 35)
+            input("Извините за неудобство, но данный момент этот номер на ремонте. С уважением администрация\n\nНажмите нажмите Enter, чтобы продолжить...")
             continue    
 
         elif booking in booked_rooms:
-            input("Ой, этот номер уже зарезервировали, выберети другой из выше перечисленных:\n\nНажмите нажмите любую клавишу, чтобы продолжить...")
+            print("-" * 35)
+            input("Ой, этот номер уже зарезервировали, выберети другой из выше перечисленных:\n\nНажмите нажмите Enter, чтобы продолжить...")
             continue
 
         elif booking in all_rooms:
             booked_rooms.append(booking)
-            input(f"Вы успешно зарезервировали номер: {booking}\n\nНажмите любую клавишу, чтобы продолжить...")
-        
-        elif booking in all_rooms:
-            booked_rooms.append(booking)
-            input(f"Вы успешно зарезервировали номер: {booking}\n\nНажмите любую клавишу, чтобы продолжить...")
+            print("-" * 35)
+            input(f"Вы успешно зарезервировали номер: {booking}\n\nНажмите Enter, чтобы продолжить...")
 
         else:   
-            input("Вы ввели не верный номер комнаты!!!\nПопробуйте снова\n\nНажмите нажмите любую клавишу, чтобы продолжить...")
+            print("-" * 35)
+            input("Вы ввели не верный номер комнаты!!!\nПопробуйте снова\n\nНажмите нажмите Enter, чтобы продолжить...")
             continue
 
     except ValueError:
-        input("Вы точно ввели не номер попробуйте снова:\n\nНажмите нажмите любую клавишу, чтобы продолжить...")
+        print("-" * 35)
+        input("Вы точно ввели не номер попробуйте снова:\n\nНажмите нажмите Enter, чтобы продолжить...")
         continue
-    
-print("\n--- ПОИСК СВОБОДНОГО НОМЕРА ---")
-while True:
-    floor = int(input("Введите номер этажа(1-5), чтобы я подобрал вам первую свободную команту на нему: "))
 
-    if floor > 0 and floor < 6:
-        #добавить цикл for который будет проходить по комнатам 1-4 и выводить номер каждой
+found = True   
+
+while found == True:
+    
+    clear_screen()
+    
+    print("--- ПОИСК СВОБОДНОГО НОМЕРА ---")
+    for f in range(1, 6):
+        print(f"Этаж {f}:", end = " ")
+        for r in range(1, 5):
+            room = (f * 100) + r
+
+            if room in booked_rooms:
+                print(f"[{room}]", end =" ")
+            else:
+                print(f"{room}", end =" ")
+        print()
+    print("-" * 30)
+     
+
+    try:
+        floor = int(input("\nДля подбора свободной комнаты введите номер этажа(1-5)\n\n(для выхода из меню напишите -> 0): "))
         
+        if floor == 0:
+            print("-" * 35)
+            print("\nДо новых встреч :)\n")
+            found = False
+        
+        elif floor > 0 and floor < 6:
+            for room in range(1, 5):
+                possible_room = (floor * 100) + room  
+                
+                if possible_room == 103 or possible_room == 304:
+                    print("-" * 35)
+                    input((f"\nИзвините за неудобства, но комната {possible_room} на данный момент на ремонте\n\nНажмите нажмите Enter, чтобы продолжить..."))
+                    continue            
+                
+                elif possible_room not in booked_rooms: 
+                    booked_rooms.append(possible_room)
+                    print("-" * 35)
+                    print(f"\nВаша новая комната {possible_room} была успешно зарезервирована!\n\nПриятного пользования :)")
+                    found = False
+                    break
+                
+                else:
+                    print("-" * 35)
+                    input(f"\nКомната {possible_room} на данный момент занята\n\nНажмите нажмите Enter, чтобы продолжить...")
+                    continue
+            print()
+        
+        else:
+            print("-" * 35)
+            input("Вы точно ввели не номер попробуйте снова:\n\nНажмите нажмите Enter, чтобы продолжить...")
+            continue
+    
+    except ValueError:
+        print("-" * 35)
+        input("Вы точно ввели не номер попробуйте снова:\n\nНажмите нажмите Enter, чтобы продолжить...")
+        continue
