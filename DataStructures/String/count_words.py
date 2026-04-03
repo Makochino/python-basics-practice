4e208ca95912a5ecf0e718c8ebb6068faa63c4f2
Max <maksimatsiuk456@gmail.com>
2026-04-03 20:11:29 +0300

    Refactored files of the String folder

--- DataStructures/String/count_words.py
+++ DataStructures/String/count_words.py
@@ -1,9 +1,9 @@
-str_1 = "Hello my name is Maksim"
+sentence = "Hello my name is Maksim"
 
-def count_words(s: str_1) -> int:
+def count_words(s: str) -> int:
     string_list = s.split()
-    return print(len(string_list))
+    return (len(string_list))
 
 
 
-count_words(str_1)
+print(count_words(sentence))
---
+++ DataStructures/String/join.py
@@ -0,0 +1,4 @@
+str_1 = "Hello my name is Maksym"
+str_1_modified = str_1.split()
+str_1_join = "_".join(str_1_modified)
+print(str_1_join)
