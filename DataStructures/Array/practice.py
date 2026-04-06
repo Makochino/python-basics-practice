685285c2ca311418399630b8b784bed18cf9edc9
Max <maksimatsiuk456@gmail.com>
2026-04-06 21:15:30 +0300

    Learnt enumerate

--- DataStructures/Array/practice.py
+++
@@ -1,6 +0,0 @@
-from array import *
-
-a = array('i', [1, 2, 3])
-b = array('i', [2, 2, 4])
-c = a + b
-print(c)
---
+++ DataStructures/Array_List/enumerate.py
@@ -0,0 +1,3 @@
+fruits = ["apple", "banana", "cherry"]
+for i, fruit in enumerate(fruits, start=1):
+    print(i, fruit)
