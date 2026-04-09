08988dc2da41545e989308eee7f85b84e078c7ae
Max <maksimatsiuk456@gmail.com>
2026-04-09 21:46:03 +0300

    study: learn enumerate and zip functions

---
+++ Lops/For_Lops/Enumerate_practice.py
@@ -0,0 +1,4 @@
+languages = ['Spanish', 'English', 'Russian', 'Chinese']
+
+for index, language in enumerate(languages, 1):
+    print(f"Number of the list: {index}\nName of the language: {language}")
