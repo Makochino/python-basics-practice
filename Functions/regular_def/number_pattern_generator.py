a443bc9d9c496f0bd701a0a92d9e0cb3f3f57011
Makochino_Laptop <maksimatsiuk456@gmail.com>
2026-04-13 11:33:40 +0300

    study: done number_pattern (must refactor)

--- Functions/regular_def/number_pattern_generator.py
+++ Functions/regular_def/number_pattern_generator.py
@@ -1,19 +1,17 @@
-n = 5
-
 def number_pattern(n):
     result = ""
     if not isinstance(n,int):
         return "Argument must be an integer value."
-    elif n == 0:
+    elif n <= 0:
         return "Argument must be an integer greater than 0."
     else:
-        for num in range(n):
+        for num in range(n+1):
             if num == 0:
                 continue
             else:
-                result += str(num)
-        print(result)
+                result += str(num) + " "
+        return result
 
-print(number_pattern(n))
+print(number_pattern(-1))
         
         
