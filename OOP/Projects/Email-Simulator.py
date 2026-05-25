f74ef0f60d3ba3fa4dff212b02a337af68faf662
Max <maksimatsiuk456@gmail.com>
2026-05-25 18:04:47 +0300

    practice: revised Email-Simulator and done some changes

--- OOP/Projects/Email-Simulator.py
+++ OOP/Projects/Email-Simulator.py
@@ -1,3 +1,5 @@
+import datetime
+
 class Email:
     def __init__(self, sender, receiver, subject, body):
         self.sender = sender
@@ -16,25 +18,25 @@ class Email:
 
     def __str__(self):
         status = "Read" if self.read else "Unread"
-        print(f"[{status}] From: {self.sender} | Subject: {self.subject} | Time: {self.timestamp.strftime('%Y-%m-%d %H:%M')}")
-        return
+        return(f"[{status}] From: {self.sender} | Subject: {self.subject} | Time: {self.timestamp.strftime('%Y-%m-%d %H:%M')}")
         
 class Inbox:
     def __init__(self):
         self.emails = []
 
     def receive_email(self, email):
-        self.emails.append(email)
+        for email in emails:
+            self.emails.append(email)
 
     def list_emails(self):
         if not self.emails:
             print("Inbox is empty.\n")
             return
         
-        print("\nYour Emails:'")
+        print("\nYour Emails: ")
         for i, email in enumerate(self.emails, start=1):
             print(f"{i}. {email}")
-            return
+        return 
 
     def read_email(self, index):
         if not self.emails:
@@ -48,4 +50,28 @@ class Inbox:
             print(f"Index that your choised is out of range")
             return
 
-    
+    def delete_email(self, index):
+        if not self.emails:
+            print("Inbox is empty.\n")
+            return 
+        
+        if index >= 0 and index < len(self.emails):
+            print(f"Email {self.emails[index]} was succefully deleted")
+            self.emails.pop(index)
+        else:
+            print(f"Index that your choised is out of range")
+            return
+
+
+#Email class checker
+email_1 = Email("Dude", "Another-Dude", "How to say Hello to your dude", "Just say hey what's up dude")
+email_1.mark_as_read()
+email_1.__str__()
+
+#Inbox class checker
+inbox_1 = Inbox()
+inbox_1.receive_email()
+inbox_1.list_emails()
+
+# Inbox should store Email objects, read_email() will fail cuz string has no Email methods, 
+# and also __str__() should return, not print(check in GPT why)
