07864afa7406efa019545da6f76da06773bfc8a9
Max <maksimatsiuk456@gmail.com>
2026-06-02 21:12:06 +0300

    practice: done some changes in Email-Simulator

--- OOP/Projects/Email-Simulator.py
+++ OOP/Projects/Email-Simulator.py
@@ -24,8 +24,8 @@ class Inbox:
     def __init__(self):
         self.emails = []
 
-    def receive_email(self, email):
-        for email in emails:
+    def receive_email(self, email_list):
+        for email in email_list:
             self.emails.append(email)
 
     def list_emails(self):
@@ -39,39 +39,62 @@ class Inbox:
         return 
 
     def read_email(self, index):
+        index -= 1
         if not self.emails:
             print("Inbox is empty.\n")
             return
 
         if index >= 0 and index < len(self.emails):
-            self.emails[index].display_full_email()
-            self.emails[index].mark_as_read()        
+            self.emails[index].mark_as_read()  
+            self.emails[index].display_full_email()      
         else:
             print(f"Index that your choised is out of range")
             return
 
     def delete_email(self, index):
+        index -= 1
         if not self.emails:
             print("Inbox is empty.\n")
             return 
         
         if index >= 0 and index < len(self.emails):
             print(f"Email {self.emails[index]} was succefully deleted")
-            self.emails.pop(index)
+            self.emails.pop(index) 
         else:
             print(f"Index that your choised is out of range")
             return
 
 
+class User():
+    def __init__(self, name, inbox):
+        self.name = name
+        self.inbox = Inbox()
+
+    def send_email(self, receiver, subject, body):
+        return None
+# User should create the Email object.
+
+# Think in real life:
+
+# John writes a letter
+# ↓
+# Letter is created
+# ↓
+# Letter is put into Mike's mailbox
+    
+
 #Email class checker
-email_1 = Email("Dude", "Another-Dude", "How to say Hello to your dude", "Just say hey what's up dude")
-email_1.mark_as_read()
-email_1.__str__()
+email_1 = Email("Dude", "Another-Dude", "How to say Hello to your dude", "Just say hey: what's up dude")
+email_2 = Email("Duder", "Another-Duder", "How to say Hello to your duder", "Just say hey: what's up duder")
+email_3 = Email("Dudler", "Another-Dudler", "How to say Hello to your dudler", "Just say: hey what's up dudler")
+
 
 #Inbox class checker
 inbox_1 = Inbox()
-inbox_1.receive_email()
-inbox_1.list_emails()
+inbox_1.receive_email([email_1, email_2, email_3])
+inbox.list_emails()
 
-# Inbox should store Email objects, read_email() will fail cuz string has no Email methods, 
-# and also __str__() should return, not print(check in GPT why)
+
+#User class checker
+
+
